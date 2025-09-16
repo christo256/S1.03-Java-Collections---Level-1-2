897f95598b16c35324e710c0e1de3987212b1800
christo256 <ckingmaster2@gmail.com>
2025-09-16 13:58:38 +0200

    ArrayList created on main class

---
+++ src/task1_S1_03_Level1/Main.java
@@ -0,0 +1,32 @@
+package task1_S1_03_Level1;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Month> months = new ArrayList<>();
+
+        months.add(new Month("January"));
+        months.add(new Month("February"));
+        months.add(new Month("March"));
+        months.add(new Month("April"));
+        months.add(new Month("May"));
+        months.add(new Month("June"));
+        months.add(new Month("July"));
+        months.add(new Month("September"));
+        months.add(new Month("October"));
+        months.add(new Month("November"));
+        months.add(new Month("December"));
+
+        System.out.println(months);
+
+        months.add(7, new Month("August"));
+
+        for ( Month month : months) {
+            System.out.println(month.getName());
+        }
+
+
+    }
+}
--- src/task1_S1_03_Level1/Month.java
+++ src/task1_S1_03_Level1/Month.java
@@ -1,5 +1,32 @@
 package task1_S1_03_Level1;
 
 public class Month {
+    private String name;
 
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Month)) return false;
+
+        Month otherMonth = (Month) obj;
+        return this.name.equals(otherMonth.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
