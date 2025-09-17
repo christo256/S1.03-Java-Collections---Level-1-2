1ad6c277f3670cca5a11eb17de194f47c16a6e30
christo256 <ckingmaster2@gmail.com>
2025-09-17 11:00:52 +0200

    HashSet and Iterator created and tested correctly

--- src/task1_S1_03_Level1/Main.java
+++ src/task1_S1_03_Level1/Main.java
@@ -1,6 +1,8 @@
 package task1_S1_03_Level1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,15 +20,25 @@ public class Main {
         months.add(new Month("October"));
         months.add(new Month("November"));
         months.add(new Month("December"));
-
-        System.out.println(months);
+        // Adding duplicate months to confirm that HashSet does not allow duplicates
+        months.add(new Month("November"));
+        months.add(new Month("December"));
 
         months.add(7, new Month("August"));
-
-        for ( Month month : months) {
+        System.out.println("Sorted ArrayList but with duplicates: ");
+        for (Month month : months) {
             System.out.println(month.getName());
         }
-
-
+        HashSet<Month> monthSet = new HashSet<>(months);
+        System.out.println("\n" + "HashSet contents without duplicates in a for loop: ");
+        for (Month month : monthSet) {
+            System.out.println(month.getName());
+        }
+        System.out.println("\n" + "Traversing the hashSet list with an iterator: ");
+        Iterator<Month> iterator = monthSet.iterator();
+        while (iterator.hasNext()) {
+            Month currentMonth = iterator.next();
+            System.out.println(currentMonth.getName());
+        }
     }
 }
